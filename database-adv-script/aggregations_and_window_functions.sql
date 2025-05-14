25c1ad85e183ff04ff2c5f9b1bd087f2b3f03865
Evidence Uche Aniowoh <aniowohevidence@gmail.com>
2025-05-14 10:47:55 +0100

    aggregation and windows function

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,10 +1,12 @@
+-- Total number of bookings per user
 SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id
 ORDER BY total_bookings DESC;
 
 
+-- Rank properties based on total bookings using ROW_NUMBER
 SELECT property_id, COUNT(*) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
 FROM bookings
 GROUP BY property_id;
